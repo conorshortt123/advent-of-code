06e441f9e91d06b74ae674a187c3d27deca639e1
Conor Shortt <conorshortt123@gmail.com>
2024-12-15 10:50:42 +0000

    Day 2 part 2

--- day-2/red-nosed-reports/java/redNosedReports.java
+++ day-2/red-nosed-reports/java/redNosedReports.java
@@ -1,43 +1,73 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class redNosedReports {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println(partOne());
+        System.out.println(runner());
     }
 
-    private static int partOne() throws FileNotFoundException {
+    private static int runner() throws FileNotFoundException {
         File file = new File("day-2/red-nosed-reports/java/input.txt");
         Scanner scanner = new Scanner(file);
         int safeReports = 0;
 
-        while(scanner.hasNextLine()) {
+        while (scanner.hasNextLine()) {
             String report = scanner.nextLine();
-            if(safeReport(report)) safeReports++;
+            if (safeReportDampener(report))
+                safeReports++;
         }
 
         return safeReports;
     }
 
-    private static boolean safeReport(String report) {
+    static boolean safeReportDampener(String report) {
         List<Integer> levelsList = Arrays.stream(report.split(" "))
                 .map(Integer::parseInt)
-                .toList();
+                .collect(Collectors.toList());
 
-        // Check levels are either increasing or decreasing by at least 1-3
+        if (safeReport(levelsList)) {
+            return true;
+        }
+
+        for (int i = 0; i < levelsList.size(); i++) {
+            List<Integer> modifiedList = new ArrayList<>(levelsList);
+            modifiedList.remove(i);
+            if (safeReport(modifiedList)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static boolean safeReport(List<Integer> levelsList) {
+        if (levelsList.size() < 2) {
+            return true;
+        }
+
         boolean increasing = levelsList.getFirst() < levelsList.getLast();
+
         for (int i = 0; i < levelsList.size() - 1; i++) {
             int current = levelsList.get(i);
-            int next = levelsList.get(i+1);
+            int next = levelsList.get(i + 1);
             int diff = Math.abs(current - next);
 
-            if(diff < 1 || diff > 3) return false;
-            if(increasing && current > next) return false;
-            if(!increasing && current < next) return false;
+            if (diff < 1 || diff > 3) {
+                return false;
+            }
+            if (increasing && current > next) {
+                return false;
+            }
+            if (!increasing && current < next) {
+                return false;
+            }
         }
+
         return true;
     }
 }
