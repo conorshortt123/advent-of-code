981159384121cbeda3cf01ba17b6b36eb91f2c85
Conor Shortt <conorshortt123@gmail.com>
2024-12-18 00:14:32 +0000

    Day 3 part 2

--- day-3/aocjava/mullItOver.java
+++ day-3/aocjava/mullItOver.java
@@ -2,36 +2,59 @@ package aocjava;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class mullItOver {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("day-3/aocjava/input.txt");
         Scanner scanner = new Scanner(file);
-        int total = 0;
 
-        while(scanner.hasNextLine()) {
-            total += partOne(scanner.nextLine());
+        List<String> lines = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            lines.add(scanner.nextLine());
         }
 
-        System.out.println(total);
+        String line = String.join("", lines);
+
+        System.out.println(partOne(line));
+        System.out.println(partTwo(line));
     }
 
-    private static int partOne(String line) {
+    private static Integer partOne(String line) {
         Pattern mulPattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)");
         Pattern extractNums = Pattern.compile("[0-9]{1,3},[0-9]{1,3}");
 
-        Optional<Integer> sum = mulPattern.matcher(line).results()
+        return mulPattern.matcher(line).results()
                 .map(match -> {
                     String[] nums = extractNums.matcher(match.group()).results().findFirst().get().group().split(",");
                     return Integer.parseInt(nums[0]) * Integer.parseInt(nums[1]);
-                }).reduce(Integer::sum);
+                }).reduce(0, Integer::sum);
+    }
 
-        return sum.get();
+    private static Integer partTwo(String line) {
+        Pattern mulPattern = Pattern.compile("mul\\([0-9]{1,3},[0-9]{1,3}\\)|do(n't)?\\(\\)");
+        Pattern extractNums = Pattern.compile("[0-9]{1,3},[0-9]{1,3}");
+        AtomicBoolean enabled = new AtomicBoolean(true);
+
+        return mulPattern.matcher(line).results()
+                .map(match -> {
+                    String matchString = match.group();
+                    if (matchString.equals("do()")) {
+                        enabled.set(true);
+                    } else if (matchString.equals("don't()")) {
+                        enabled.set(false);
+                    } else {
+                        String[] actions = extractNums.matcher(matchString).results()
+                                .findFirst().get().group().split(",");
+                        if (enabled.get()) {
+                            return Integer.parseInt(actions[0]) * Integer.parseInt(actions[1]);
+                        }
+                    }
+                    return 0;
+                }).reduce(0, Integer::sum);
     }
 }
